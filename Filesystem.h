52f425ab879e14ca27d3873be9bd060db69f85dd
Dr. Douglas Fulton Shaw <xhd2015@gmail.com>
2017-02-28 12:47:12 +0800

    added stdc & stdc++ for later support

---
+++ Filesystem.h
@@ -0,0 +1,54 @@
+
+#ifndef Filesystem_h
+#define Filesystem_h
+
+/**
+*using policy designate
+*
+*	The common part of NTFS,FAT,ext... filesystems:
+*		
+*/
+class FileSystem{
+public:
+	//functions that must be implemented
+
+
+
+protected:
+
+
+
+};
+
+/**
+*	implement the ext4-linux file system
+*/
+class Ext4FileSystem{
+public:
+
+
+protected:	
+
+
+
+};
+
+/**
+*	implement the FAT file system
+*/
+class FATFileSystem{
+
+
+};
+
+
+/**
+*	implement SimpleFileSystem
+*		as it is simple filesystem,most kernel can use it with as few modifications as possible
+*/
+
+
+
+
+
+#endif //Filesystem_h
